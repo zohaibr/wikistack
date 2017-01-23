6884f1b11cc5692fc49e82638c764129aaeab59a
Jessica Perina <jperina@gmail.com>
2017-01-23 12:00:23 -0500

    added sequelize models

--- app.js
+++ app.js
@@ -7,21 +7,34 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 const nunjucks = require('nunjucks');
 const path = require('path');
+const models = require('./models');
 
-
+// templating boilerplate setup
 app.engine('html', nunjucks.render);
 app.set('view engine', 'html');
 nunjucks.configure('views', ({noCache: true}));
 
+// logging middleware
 app.use(volleyball);
 
+// start the server
 app.use(bodyParser.urlencoded ({ extended: true }));
 app.use(bodyParser.json());
 
-app.listen(3000, function() {
-    console.log('listening on 3000');
-});
+// sync models with database
+models.User.sync({})
+.then(function() {
+	return models.Page.sync({});
+})
+.then(function() {
+	app.listen(3000, function() {
+	    console.log('listening on 3000');
+	});
+})
+.catch(console.error);
+
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+// modular routing that uses io inside it
 app.use('/', routes);
--- models/index.js
+++ models/index.js
@@ -1,2 +1,45 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+		logging: false
+});
+
+var Page = db.define('page', {
+	title: {
+		type: 		Sequelize.STRING,
+		allowNull:  false
+	},
+	urlTitle: {
+		type: 		Sequelize.STRING,
+		allowNull:  false,
+		isUrl: 		true,
+	},
+	content: {
+		type: 		Sequelize.TEXT,
+		allowNull:  false
+	},
+	status: 		Sequelize.ENUM('open', 'closed'),
+	date: {
+		type: 		Sequelize.DATE,
+		defaultValue: Sequelize.NOW
+	}
+	}, {
+			getUrl:  {
+				siteUrl: function() { return this.getDataValue(`/wiki/${urlTitle}`);}
+			}
+});
+
+var User = db.define('user', {
+	name: {
+		type: 		Sequelize.STRING,
+		allowNull:  false
+	},
+	email: {
+		type: 		Sequelize.STRING,
+		allowNull:  false,
+		isEmail: 	true
+	}
+});
+module.exports = {
+	Page: Page,
+	User: User
+};
